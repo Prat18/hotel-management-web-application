47409d9df156e994834aa57f636ef2ede478d8b6
Prat18 <pratyush181996@gmail.com>
2018-03-27 00:30:21 +0530

    server-setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ExecutiveComponent } from './components/executive/executive.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'dashboard', component: DashboardComponent},
+    { path: 'register', component: RegisterComponent},
+    {path: 'executive', component: ExecutiveComponent},
     {path: '**', component: HomeComponent}
 ]
 
